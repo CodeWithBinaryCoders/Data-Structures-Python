b14bf75723f23017dde0d5c25eeea2d5e034a656
Avik Agarwala <avikagarwala@outlook.com>
2023-11-15 20:01:53 +0530

    Create singly linked list insert at beginning

---
+++ linked-list/singly-linked-list/insert-at-beginning.py
@@ -0,0 +1,29 @@
+# Linked list in python
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Linked_list:
+    __head = None
+
+    def insert_at_first(self, data):
+        new_node = Node(data)
+        new_node.next = self.__head
+        self.__head = new_node
+
+    def display(self):
+        temp = self.__head
+        while temp is not None: # while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+
+
+my_list = Linked_list()
+
+my_list.insert_at_first(1)
+my_list.insert_at_first(2)
+my_list.insert_at_first(3)
+
+my_list.display()
