8ed19786aa8734f141bbe18fe98d69265b095172
Avik Agarwala <avikagarwala@outlook.com>
2023-11-22 17:01:40 +0530

    reverse print of singly linked list

---
+++ linked-list/singly-linked-list/recursive-print/reverse-print.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Linked_list:
+    __head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+
+        new_node.next = self.__head
+        self.__head = new_node
+
+    def __recursive_print(self, pointer):
+        if not pointer: # if list is empty
+            return
+        
+        self.__recursive_print(pointer.next)
+
+        print(pointer.data, end = ' ')
+
+    def reverse_print(self):
+        self.__recursive_print(self.__head)
+
+        print()
+
+        
+
+my_list = Linked_list()
+
+my_list.insert_at_beginning(5) # 5
+my_list.insert_at_beginning(4) # 4 5
+my_list.insert_at_beginning(3) # 3 4 5
+my_list.insert_at_beginning(2) # 2 3 4 5
+my_list.insert_at_beginning(1) # 1 2 3 4 5
+
+my_list.reverse_print() # 5 4 3 2 1
